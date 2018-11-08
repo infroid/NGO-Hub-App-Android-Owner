79a790fede5cad8dd50a45c2004d2e07a534fce6
Paras Rawat <parasmyname@gmail.com>
2018-11-08 19:57:34 +0530

    Volunteer Login Adjustment

--- app/src/main/java/com/example/parasrawat2124/ngo/Adapters/RequestRecyclerAdapter.java
+++ app/src/main/java/com/example/parasrawat2124/ngo/Adapters/RequestRecyclerAdapter.java
@@ -1,15 +1,19 @@
 package com.example.parasrawat2124.ngo.Adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.parasrawat2124.ngo.R;
+import com.example.parasrawat2124.ngo.RequestDetails;
 
 import java.util.ArrayList;
 
@@ -33,9 +37,21 @@ public class RequestRecyclerAdapter extends RecyclerView.Adapter<RequestRecycler
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RequestRecyclerAdapter.ViewHolder viewHolder, int i) {
+    public void onBindViewHolder(@NonNull RequestRecyclerAdapter.ViewHolder viewHolder, final int i) {
         viewHolder.imageView.setImageResource(immagearraylist.get(i));
         viewHolder.desc.setText(descriptionarraylit.get(i));
+        viewHolder.requestrelativelayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos=i;
+                String description=descriptionarraylit.get(i);
+                Intent intent=new Intent(context,RequestDetails.class);
+                intent.putExtra("description",description);
+                intent.putExtra("position",pos);
+                intent.putIntegerArrayListExtra("imagearraylist",immagearraylist);
+                context.startActivity(intent);
+            }
+        });
 
     }
 
@@ -46,11 +62,13 @@ public class RequestRecyclerAdapter extends RecyclerView.Adapter<RequestRecycler
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         ImageView imageView;
+        CardView requestrelativelayout;
         TextView desc;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             imageView=itemView.findViewById(R.id.recyclerimageview);
             desc=itemView.findViewById(R.id.recyclerdescription);
+            requestrelativelayout=itemView.findViewById(R.id.requestrelativelayout);
         }
     }
 }
